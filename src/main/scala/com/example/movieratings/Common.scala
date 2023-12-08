1f268f3bb55209e229d3f68b019aea5a20ab4dc8
Oleksii Hytsiv <oleksii.hytsiv@gmail.com>
2023-12-08 23:24:59 +0200

    Refactor code to use `CSVRecord`

---
+++ src/main/scala/com/example/movieratings/Common.scala
@@ -0,0 +1,57 @@
+package com.example.movieratings
+
+import java.io.File
+import org.apache.commons.csv.CSVRecord
+
+object Common {
+
+  def generateReport(
+      movies: Map[String, List[String]],
+      reviews: List[File]
+  ): List[List[Any]] = {
+    reviews
+      .map { file =>
+        val (movieId, reviews) = readReviews(file)
+        getRatedEntry(movieId, reviews, movies)
+      }
+  }
+
+  def getRatedEntry(
+      movieId: String,
+      reviews: List[CSVRecord],
+      movies: Map[String, List[String]]
+  ): List[Any] = {
+    val movie = movies.get(movieId)
+    val averageRating = calcAverageRating(reviews)
+    val totalReviews = reviews.size
+
+    List(movie.get(1), movie.get(0), averageRating, totalReviews)
+  }
+
+  def calcAverageRating(ratings: List[CSVRecord]): Double = {
+    val sum = ratings.map(_.get(1).toDouble).sum
+    sum / ratings.size
+  }
+
+  def readMovies(path: File): List[CSVRecord] = {
+    CsvUtils
+      .readFromFileAsList(path)
+      .filter(m => !m.get(1).isNullOrEmpty)
+  }
+
+  def readReviews(path: File): (String, List[CSVRecord]) = {
+    val entries = CsvUtils
+      .readFromFileAsList(path)
+    val movieId = entries.head.get(0).dropRight(1)
+    val reviews = entries.tail
+    (movieId, reviews)
+  }
+
+  def getMovieRatingsPath(dir: String, movieId: String): String =
+    dir + File.separator + "mv_" + ("0" * (7 - movieId.length)) + movieId + ".txt"
+
+  implicit class StringExtensions(s: String) {
+    def isNullOrEmpty: Boolean =
+      s == null || s.isEmpty || s.toLowerCase == "null"
+  }
+}
--- src/main/scala/com/example/movieratings/ReportGenerator.scala
+++ src/main/scala/com/example/movieratings/ReportGenerator.scala
@@ -15,24 +15,22 @@ object ReportGenerator {
   def main(args: Array[String]): Unit = {
     val Array(movieTitlesPath, trainingSetPath, reportPath) = args
 
-    val movies = readFromFileAsList(new File(movieTitlesPath))
+    val movies = readMovies(new File(movieTitlesPath))
       .filter(m =>
-        !m.get(1).isNullOrEmpty
-          && m.get(1).toInt >= yearBounds._1
+        m.get(1).toInt >= yearBounds._1
           && m.get(1).toInt <= yearBounds._2
       )
-      .map(_.values)
 
     val ratingFiles = movies
-      .map(m => Try(new File(getMovieRatingsPath(trainingSetPath, m(0)))))
+      .map(m => Try(new File(getMovieRatingsPath(trainingSetPath, m.get(0)))))
       .filter(_.isSuccess)
       .map(_.get)
 
     val report = generateReport(
-      movies.map(m => (m(0), Array(m(1), m(2)))).toMap,
+      movies.map(m => (m.get(0), List(m.get(1), m.get(2)))).toMap,
       ratingFiles,
-      minRatings
-    )
+    ).filter(_.last.asInstanceOf[Int] >= minRatings)
+    .sortBy(r => (-r(2).asInstanceOf[Double], r(0).asInstanceOf[String]))
 
     writeToFile(report, new File(reportPath))
   }
